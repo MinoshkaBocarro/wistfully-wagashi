9578fb88ae066c5bf0738f0a6008e1049db1c3eb
mb <115391907+MinoshkaBocarro@users.noreply.github.com>
2026-03-08 20:57:50 +1100

    Attempt to fix deployment issues

--- server/src/config/config.js
+++ server/src/config/config.js
@@ -5,9 +5,19 @@ module.exports = {
 	// Database Envs
 	db: {
 		serviceAccountKey: {
-			private_key: process.env.FIREBASE_PRIVATE_KEY,
-			project_id: process.env.FIREBASE_PROJECT_ID,
-			client_email: process.env.FIREBASE_CLIENT_EMAIL,
+			type: process.env.SERVICE_ACCOUNT_KEY_TYPE,
+			project_id: process.env.SERVICE_ACCOUNT_KEY_PROJECT_ID,
+			private_key_id: process.env.SERVICE_ACCOUNT_KEY_PRIVATE_KEY_ID,
+			private_key: process.env.SERVICE_ACCOUNT_KEY_PRIVATE_KEY,
+			client_email: process.env.SERVICE_ACCOUNT_KEY_CLIENT_EMAIL,
+			client_id: process.env.SERVICE_ACCOUNT_KEY_CLIENT_ID,
+			auth_uri: process.env.SERVICE_ACCOUNT_KEY_AUTH_URI,
+			token_uri: process.env.SERVICE_ACCOUNT_KEY_TOKEN_URI,
+			auth_provider_x509_cert_url:
+				process.env.SERVICE_ACCOUNT_KEY_AUTH_PROVIDER,
+			client_x509_cert_url:
+				process.env.SERVICE_ACCOUNT_KEY_CLIENT_CERT_URL,
+			universe_domain: process.env.SERVICE_ACCOUNT_KEY_UNIVERSE_DOMAIN,
 		},
 	},
 
